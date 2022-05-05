9e924b3695def9b954774fa5db569c45c39a730f
Garrett Tallent <garesgaming@gmail.com>
2022-05-05 17:19:47 -0400

    Admin menu styling. File upload form & ajax func

--- admin/home.php
+++ admin/home.php
@@ -6,56 +6,61 @@ if(empty($user)){
 <div class="l-navbar">
     <h1> Admin Menu </h1>
     <a href="auth/logout.php"><h1>log out</h1></a>
-
 <?php #display galleries
+#check for http get request named "gallery"
+#if found, then show the images in that gallery
+if(isset($_GET['gallery'])){
+    #echo the back button to allow user to go back to menu of galleries.
+    echo '<a href="./"><h1>go back to galleries</h1></a>';
+    #now echo the closing tag for the navbar and opening tag for the gallery list
+    echo '</div> <div class="l-galleries-list">';
+    $images = glob('../imgs/galleries/'.$_GET['gallery'].'/*.{jpg,png,gif}', GLOB_BRACE);
+    foreach($images as $image){
+        echo('<div class="gallery-item">');
+        #print image
+        echo('<img src="' . $image . '">');
 
-    #check for http get request named "gallery"
-    #if found, then show the images in that gallery
-    if(isset($_GET['gallery'])){
-        #echo the back button to allow user to go back to menu of galleries.
-        echo '<a href="./"><h1>go back to galleries</h1></a>';
-        #now echo the closing tag for the navbar and opening tag for the gallery list
-        echo '</div> <div class="l-galleries-list">';
-        $images = glob('../imgs/galleries/'.$_GET['gallery'].'/*.{jpg,png,gif}', GLOB_BRACE);
-        foreach($images as $image){
-            echo('<div class="gallery-item">');
-            #print image
-            echo('<img src="' . $image . '">');
+        #delete button
 
-            #delete button
-
-            echo('</div>');
-        }
-
-        #add new images button
-        echo '<div class="gallery-item">
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select image(s) to upload:
-        <input type="file" name="filesToUpload" id="filesToUpload" multiple>
-        <input type="submit" value="Upload Image(s)" name="submit">
-        </form>
-        </div>';
+        echo('</div>');
     }
-    #if none, then...
-    else{
-        #echo the rest of the navbar (not needed for this portion)
-        echo '</div> <div class="l-galleries-list">';
 
-        #for each folder in "imgs/galleries/", generate a link. 
-        #If link is clicked, use GET http request with gallery number
-        $scan = scandir('../imgs/galleries/');
-        $files = array_diff($scan, array('.', '..'));
-        
-        foreach($files as $file) {
-            if (is_dir("../imgs/galleries/$file")) {
-                echo '<div class="gallery-folder">';
-                echo '<a href="'.'./?'.http_build_query(array('gallery' => $file)).'">';
-                echo $file;
-                echo '</a></div>';
-            }
-        }
-    }
+    #add new images button
+    echo '<div class="gallery-item">
+    Select image(s) to upload:
+    <input type="file" name="filesToUpload" id="filesToUpload" accept=".png, .jpg, .gif" multiple>
+    <input type="submit" value="Upload Image(s)" name="submit" onclick="uploadImages()">
+    </div>';
+}
+#if none, then...
+else{
+    #echo the rest of the navbar (not needed for this portion)
+    echo '</div> <div class="l-galleries-list">';
+
+    #for each folder in "imgs/galleries/", generate a link. 
+    #If link is clicked, use GET http request with gallery number
+    $scan = scandir('../imgs/galleries/');
+    $files = array_diff($scan, array('.', '..'));
     
+    foreach($files as $file) {
+        if (is_dir("../imgs/galleries/$file")) {
+            echo '<div class="gallery-folder">';
+            echo '<a href="'.'./?'.http_build_query(array('gallery' => $file)).'">';
+            echo $file;
+            echo '</a></div>';
+        }
+    }
+}
 ?>
-
+<script>
+async function uploadImages(){
+    let formData = new FormData();
+    formData.append("file", filesToUpload.files);
+    await fetch('/upload.php', {
+        method: "POST",
+        body: formData
+    });
+    alert('Your images have been uploaded successfully.');
+}
+</script>
 </div>
