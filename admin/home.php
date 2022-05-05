e142bc6a30603bbe851ecc58b8fc09e24d4c26a6
Garrett Tallent <garesgaming@gmail.com>
2022-05-04 23:43:59 -0400

    styling added for admin menu

--- admin/home.php
+++ admin/home.php
@@ -3,10 +3,13 @@ if(empty($user)){
     header("Location:index.php"); exit();
 }
 ?>
+<div class="l-navbar">
+    <h1> Tammy Hudson Photography </h1>
+    <h1> Admin Menu </h1>
+    <a href="auth/logout.php"><h1>log out</h1></a>
+</div>
 
-<h1>Welcome home <?php echo $user['username']; ?></h1>
-<p>You are logged in!</p>
-<p>Click here to <a href="auth/logout.php">log out</a></p>
+
 
 <?php #display galleries
     #for each folder in "imgs/galleries/", generate a link. If link is clicked, use GET http request with gallery number
