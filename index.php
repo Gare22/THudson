7e57c57ba02f36c9363b15a89d1c18f0e7695a69
Garrett Tallent <garesgaming@gmail.com>
2022-05-07 02:25:22 -0400

    gallery scrolls correctly and infinitely no jitter

--- index.php
+++ index.php
@@ -147,35 +147,8 @@
     }   
 </script>
 
-<script>
-const style = document.createElement('style');
-style.innerHTML = `
-.gallery-items:first-child{
-    animation: bannermove 15s linear infinite;
-}
+<script src="js/gallery.js" defer>
 
-
-@keyframes bannermove {
-    0% {
-        margin-left: 0px;
-    }
-    100% {
-        margin-left: -`+ getGalleryWidth() +`px;
-    }
-}`
-
-function getGalleryWidth(){
-    var children = document.getElementById('home-gallery').children;
-    var totalWidth = 0;
-
-    for (var i = 0; i < children.length-2; i++) {
-        totalWidth += parseInt(children[i].offsetWidth, 10);
-    }
-
-    return totalWidth;
-}
-
-document.head.appendChild(style);
 </script>
 
 </html>
---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+const style = document.createElement('style');
+style.innerHTML = `
+.gallery-items:first-child{
+    animation: bannermove 15s linear infinite;
+}
+
+
+@keyframes bannermove {
+    0% {
+        margin-left: 0px;
+    }
+    100% {
+        margin-left: -`+ getGalleryWidth() +`px;
+    }
+}`
+
+function getGalleryWidth(){
+    var children = document.getElementById('home-gallery').children;
+    var totalWidth = 0;
+
+    for (var i = 0; i < children.length-2; i++) {
+        totalWidth += parseInt(children[i].offsetWidth, 10);
+    }
+
+    return totalWidth;
+}
+
+document.head.appendChild(style);
